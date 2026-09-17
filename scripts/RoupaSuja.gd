38dff9d101b39f8e7a07ecc58f6ef82c0be7dd25
Vilela <viniciusvlopes@outlook.com>
2026-09-17 15:30:50 -0300

    Mini-game Roupas

--- scripts/RoupaSuja.gd
+++ scripts/RoupaSuja.gd
@@ -4,3 +4,10 @@ extends Node
 @onready var roupaSuja: Sprite2D = $Roupa
 
 @onready var sprite_barra: Sprite2D = $SpriteBarra
+
+func _ready():
+	input_event.connect(_on_input_event)
+	
+func _on_input_event(viewport, event, shape_idx)
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		$Roupa.visible = false
