71de082f9515e62d826f98c23aa3731405305661
Vilela <viniciusvlopes@outlook.com>
2026-09-17 16:01:37 -0300

    Minigame Roupa

--- scripts/RoupaSuja.gd
+++ scripts/RoupaSuja.gd
@@ -1,13 +1,15 @@
-extends Node
+extends Area2D
 
 @onready var cestoRoupa: Sprite2D = $Cesto
 @onready var roupaSuja: Sprite2D = $Roupa
 
-@onready var sprite_barra: Sprite2D = $SpriteBarra
-
 func _ready():
 	input_event.connect(_on_input_event)
-	
-func _on_input_event(viewport, event, shape_idx)
+
+func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		$Roupa.visible = false
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
